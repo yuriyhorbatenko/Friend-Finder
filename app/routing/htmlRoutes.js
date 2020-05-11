eff16430d3dcabf1f470427608bb8024fe6b3677
Yuriy <xett03@gmail.com>
2020-05-11 16:49:41 -0400

    app almost ready

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,10 +4,10 @@ var path = require("path");
 module.exports = function (app) {
 
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
