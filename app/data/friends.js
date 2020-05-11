bb672dbeb783d2fa55709d852675c12e271ed701
Yuriy <xett03@gmail.com>
2020-05-11 16:45:37 -0400

    app almost ready

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,85 @@
 var friends = [
     {
-        name: "Rachel Green",
+        name: "Rachel G.",
         photo: "https://api.time.com/wp-content/uploads/2019/09/rachel-green-friends-ralph-lauren-collection.jpg",
         scores: [
-            "5",
-            "2",
-            "2",
-            "2",
-            "4",
-            "1",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
             "3"
+        ]
+    },
+
+    {
+        name: "Joey T.",
+        photo: "https://www.cahsofficial.com/wp-content/uploads/2019/11/540599ec-b13a-428c-8075-6f15bda099d6-joey-tribbianni-best-pickup-lines.png",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+
+    {
+        name: "Ross G.",
+        photo: "https://www.tracking-board.com/wp-content/uploads/2017/12/ross-geller.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+
+    {
+        name: "Phoebe B.",
+        photo: "https://lgbtfansdeservebetter.com/static/uploads/2016/09/Phoebe-Buffay-Lisa-Kudrow.png",
+        scores: [
+            "5",
+            "3",
+            "5",
+            "3",
+            "5",
+            "3",
+            "5"
+        ]
+    },
+
+    {
+        name: "Chandler B.",
+        photo: "https://www.stylist.co.uk/images/app/uploads/2017/10/24195115/34fb7869e89b21c70d0662c6af04fb53-828x552.jpg?w=1200&h=1&fit=max&auto=format%2Ccompress",
+        scores: [
+            "1",
+            "3",
+            "1",
+            "3",
+            "1",
+            "3",
+            "1"
+        ]
+    },
+
+    {
+        name: "Monica G.",
+        photo: "https://youthincmag.com/wp-content/uploads/2020/02/Hall-Of-Series-Monica-696x391.jpg",
+        scores: [
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1"
         ]
     }
 ];
