7b7fb3e994f6117d3e70a22c1a929a04cb8a6761
Yuriy <xett03@gmail.com>
2020-05-10 22:39:34 -0400

    working on code

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+    {
+        name: "Rachel Green",
+        photo: "https://api.time.com/wp-content/uploads/2019/09/rachel-green-friends-ralph-lauren-collection.jpg",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
